944f06701fc8abf3e9cf90bcc0c733ba34fad7e9
Gabe Byrne <byrnegr@gmail.com>
2020-09-18 17:54:44 -0400

    first commit

---
+++ webpack.config.jsx
@@ -0,0 +1,85 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const outputDir = "./dist";
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/northone.jsx",
+  output: {
+    path: path.resolve(__dirname, "app", "assets", "javascripts"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx", "*"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          query: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/react",
+              {
+                plugins: ["@babel/plugin-proposal-class-properties"],
+              },
+            ],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // you can specify a publicPath here
+              // by default it uses publicPath in webpackOptions.output
+              publicPath: "../",
+              hmr: process.env.NODE_ENV === "development",
+            },
+          },
+          "css-loader",
+          "postcss-loader",
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              // you can specify a publicPath here
+              // by default it uses publicPath in webpackOptions.output
+              name: "[name].[ext]",
+              outputPath: "assets/images/",
+              publicPath: "assets/images/",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.scss/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // you can specify a publicPath here
+              // by default it uses publicPath in webpackOptions.output
+              publicPath: "../",
+              hmr: process.env.NODE_ENV === "development",
+            },
+          },
+          "css-loader",
+          "sass-loader",
+          "postcss-loader",
+        ],
+      },
+    ],
+  },
+
+  devtool: "source-map",
+};
